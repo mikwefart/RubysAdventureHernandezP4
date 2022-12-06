b52896dd3e543e525b7e61fcbc041b23dcce078e
mikwefart <116204698+mikwefart@users.noreply.github.com>
2022-12-06 11:43:34 -0800

    fixed jittering code

--- RubysAdventureHernandez/Assets/Scripts/RubyController.cs
+++ RubysAdventureHernandez/Assets/Scripts/RubyController.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //Creat my movemment vector
-        Vector2 position = transform.position;
-
-        //create the movement of charater
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //set the new position
-        transform.position = position;
-
-
+        rigidbody2d.MovePosition(position);
     }
 }
