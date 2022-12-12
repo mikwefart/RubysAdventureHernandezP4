97eb430ba229d79248c4aa6d6195642a317c7d6f
mikwefart <116204698+mikwefart@users.noreply.github.com>
2022-12-12 11:44:52 -0800

    wrote more scripts

--- RubysAdventureHernandez/Assets/Scripts/RubyController.cs
+++ RubysAdventureHernandez/Assets/Scripts/RubyController.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //create 2 variables to use the unity built in axes
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
     }
-
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -28,5 +32,10 @@ public class RubyController : MonoBehaviour
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
